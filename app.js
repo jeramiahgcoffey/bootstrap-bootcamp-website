bd70e7554f4a1d5b10a6836280566067f2c3cd1f
jeramiahgcoffey <jerrygcoffey@gmail.com>
2022-04-05 13:28:42 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+    'pk.eyJ1IjoiamVyYW1pYWhjb2ZmZXkiLCJhIjoiY2wxbWc2MTI5MGszYTNlcnphNTMybXJ2cSJ9.FgzQayVLbi_w4Ud85b5b9Q';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.060982, 42.35725],
+    zoom: 18,
+});
